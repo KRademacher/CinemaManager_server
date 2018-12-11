c125fc34175dca5d9220263eef673aa11211c927
KRademacher <KRademacher@users.noreply.github.com>
2018-12-11 23:35:18 +0100

    Changed db connection

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {};
 
 config.mongoURI = {
-	development: 'mongodb://localhost/cinema',
-  //development: 'mongodb://admin:admin12345@ds231374.mlab.com:31374/cinema',
+	//development: 'mongodb://localhost/cinema',
+  development: 'mongodb://admin:admin12345@ds231374.mlab.com:31374/cinema',
   //test: 'mongodb://localhost/cinema_test'
   test: 'mongodb://admin:admin12345@ds231374.mlab.com:31374/cinema_test'
 };
